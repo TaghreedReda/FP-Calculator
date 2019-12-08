fcb1e2a63b10e4d84d62716406352002e14290b7
TaghreedReda <taghreedzredaz@gmail.com>
2019-12-08 21:47:38 +0200

    adding UFP

---
+++ Metrics Project/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace Metrics_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.EI_Box = new System.Windows.Forms.TextBox();
+            this.EO_Box = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.EQ_Box = new System.Windows.Forms.TextBox();
+            this.ILF_Box = new System.Windows.Forms.TextBox();
+            this.EIF_Box = new System.Windows.Forms.TextBox();
+            this.DI_Box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EI_Box
+            // 
+            this.EI_Box.Location = new System.Drawing.Point(250, 44);
+            this.EI_Box.Name = "EI_Box";
+            this.EI_Box.Size = new System.Drawing.Size(100, 22);
+            this.EI_Box.TabIndex = 0;
+            this.EI_Box.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // EO_Box
+            // 
+            this.EO_Box.Location = new System.Drawing.Point(250, 95);
+            this.EO_Box.Name = "EO_Box";
+            this.EO_Box.Size = new System.Drawing.Size(100, 22);
+            this.EO_Box.TabIndex = 1;
+            this.EO_Box.TextChanged += new System.EventHandler(this.EO_Box_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
+            // 
+            // EQ_Box
+            // 
+            this.EQ_Box.Location = new System.Drawing.Point(250, 141);
+            this.EQ_Box.Name = "EQ_Box";
+            this.EQ_Box.Size = new System.Drawing.Size(100, 22);
+            this.EQ_Box.TabIndex = 5;
+            this.EQ_Box.TextChanged += new System.EventHandler(this.EQ_Box_TextChanged);
+            // 
+            // ILF_Box
+            // 
+            this.ILF_Box.Location = new System.Drawing.Point(250, 187);
+            this.ILF_Box.Name = "ILF_Box";
+            this.ILF_Box.Size = new System.Drawing.Size(100, 22);
+            this.ILF_Box.TabIndex = 6;
+            this.ILF_Box.TextChanged += new System.EventHandler(this.ELF_Box_TextChanged);
+            // 
+            // EIF_Box
+            // 
+            this.EIF_Box.Location = new System.Drawing.Point(250, 242);
+            this.EIF_Box.Name = "EIF_Box";
+            this.EIF_Box.Size = new System.Drawing.Size(100, 22);
+            this.EIF_Box.TabIndex = 7;
+            this.EIF_Box.TextChanged += new System.EventHandler(this.EIF_Box_TextChanged);
+            // 
+            // DI_Box
+            // 
+            this.DI_Box.Location = new System.Drawing.Point(250, 286);
+            this.DI_Box.Name = "DI_Box";
+            this.DI_Box.Size = new System.Drawing.Size(100, 22);
+            this.DI_Box.TabIndex = 8;
+            this.DI_Box.TextChanged += new System.EventHandler(this.DI_Box_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "DI";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "External Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "External Output";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "External Interface Files";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Internal Logical Files";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "External Inquiry";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 396);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Calculate UFP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 425);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(253, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Calculate TCF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 454);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(253, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Calculate FP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(410, 425);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 18;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(410, 396);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 19;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(410, 454);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 20;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(452, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 21;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(452, 97);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 22;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(452, 143);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 23;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(452, 185);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 24;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(452, 235);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 24);
+            this.comboBox5.TabIndex = 25;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(667, 401);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 17);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "LOC";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 489);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DI_Box);
+            this.Controls.Add(this.EIF_Box);
+            this.Controls.Add(this.ILF_Box);
+            this.Controls.Add(this.EQ_Box);
+            this.Controls.Add(this.EO_Box);
+            this.Controls.Add(this.EI_Box);
+            this.Name = "Form1";
+            this.Text = "+";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EI_Box;
+        private System.Windows.Forms.TextBox EO_Box;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.TextBox EQ_Box;
+        private System.Windows.Forms.TextBox ILF_Box;
+        private System.Windows.Forms.TextBox EIF_Box;
+        private System.Windows.Forms.TextBox DI_Box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Metrics Project/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metrics_Project
+{
+    public partial class Form1 : Form
+    {
+        public static int EI=0;
+        public static int EO=0;
+        public static int EQ = 0;
+        public static int ILF = 0;
+        public static int EIF = 0;
+        public static int DI = 0;
+        public static int UFP = 0;
+        public static double TCF = 0;
+        public static double FP = 0
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(EI_Box.Text!="")
+            EI = Convert.ToInt32(EI_Box.Text);
+        }
+
+        private void EO_Box_TextChanged(object sender, EventArgs e)
+        {
+            if (EO_Box.Text != "")
+                EO = Convert.ToInt32(EO_Box.Text);
+        }
+
+        private void EIF_Box_TextChanged(object sender, EventArgs e)
+        {
+            if (EIF_Box.Text != "")
+                EIF = Convert.ToInt32(EIF_Box.Text);
+        }
+
+        private void ELF_Box_TextChanged(object sender, EventArgs e)
+        {
+            if (ILF_Box.Text != "")
+                ILF = Convert.ToInt32(ILF_Box.Text);
+        }
+
+        private void EQ_Box_TextChanged(object sender, EventArgs e)
+        {
+            if (EQ_Box.Text != "")
+                EQ = Convert.ToInt32(EQ_Box.Text);
+        }
+
+        private void DI_Box_TextChanged(object sender, EventArgs e)
+        {
+            DI = Convert.ToInt32(DI_Box.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "Simple")
+                EI *= 3;
+            else if (comboBox1.SelectedItem.ToString() == "Average")
+                EI *= 4;
+            else if (comboBox1.SelectedItem.ToString() == "Complex")
+                EI *= 6;
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem.ToString() == "Simple")
+                EO *= 4;
+            else if (comboBox2.SelectedItem.ToString() == "Average")
+                EO *= 5;
+            else if (comboBox2.SelectedItem.ToString() == "Complex")
+                EO *= 7;
+
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (comboBox3.SelectedItem.ToString() == "Simple")
+                EQ *= 3;
+            else if (comboBox3.SelectedItem.ToString() == "Average")
+                EQ*= 4;
+           else if (comboBox3.SelectedItem.ToString() == "Complex")
+                EQ *= 6;
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox4.SelectedItem.ToString() == "Simple")
+                ILF *= 7;
+            if (comboBox4.SelectedItem.ToString() == "Average")
+                ILF *=10;
+            if (comboBox4.SelectedItem.ToString() == "Complex")
+                ILF *= 15;
+
+
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox5.SelectedItem.ToString() == "Simple")
+                EIF *= 5; 
+                if (comboBox5.SelectedItem.ToString() == "Average")
+                EIF *= 7;
+            if (comboBox5.SelectedItem.ToString() == "Complex")
+                EIF *= 10;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            comboBox1.Items.Add("Simple");
+            comboBox1.Items.Add("Average");
+            comboBox1.Items.Add("Complex");
+            this.Controls.Add(comboBox1);
+
+            comboBox2.Items.Add("Simple");
+            comboBox2.Items.Add("Average");
+            comboBox2.Items.Add("Complex");
+            this.Controls.Add(comboBox2);
+
+            comboBox3.Items.Add("Simple");
+            comboBox3.Items.Add("Average");
+            comboBox3.Items.Add("Complex");
+            this.Controls.Add(comboBox3);
+
+            comboBox4.Items.Add("Simple");
+            comboBox4.Items.Add("Average");
+            comboBox4.Items.Add("Complex");
+            this.Controls.Add(comboBox4);
+
+            comboBox5.Items.Add("Simple");
+            comboBox5.Items.Add("Average");
+            comboBox5.Items.Add("Complex");
+            this.Controls.Add(comboBox5);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UFP = EI + EO + EQ + ILF + EIF;
+            textBox8.Text = UFP.ToString();
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TCF = 0.65 + 0.01 * DI;
+            textBox7.Text = TCF.ToString();
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FP = TCF * UFP;
+            textBox9.Text = FP.ToString();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
